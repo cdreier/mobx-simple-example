0642bfc22a3762e2e7afeeb165f087d32bf0a677
Christian Dreier <christian.dreier@inovex.de>
2019-12-10 10:07:42 +0100

    adding decorator support

--- src/store/TodoModel.ts
+++ src/store/TodoModel.ts
@@ -1,20 +1,8 @@
 import {decorate, observable} from 'mobx'
 
 class TodoModel {
-  text = ''
-  completed = false
+  @observable text = ''
+  @observable completed = false
 }
-
-// note: in create-react-app, there is no decorator support
-// https://mobx.js.org/best/decorators.html
-decorate(TodoModel, {
-  text: observable,
-  completed: observable,
-})
-// if you set up your own project, or eject the react-app
-// you can add the observable decorator to the field itself
-// class TodoModel {
-//   @observable text = ''
-// }
 
 export default TodoModel
